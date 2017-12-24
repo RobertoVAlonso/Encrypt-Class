9366ccac345f63594bfdbc036fb1c36d5d5fc765
Roberto Alonso <thebest1949@yahoo.com>
2017-12-24 15:03:28 -0500

    Initial Commit

---
+++ Encrypt Class/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Encrypt Class
+//
+//  Created by Roberto Alonso on 12/24/17.
+//  Copyright © 2017 Roberto Alonso. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
